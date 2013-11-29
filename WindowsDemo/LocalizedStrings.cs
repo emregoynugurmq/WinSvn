fc58763ea7ad3c25afc8abf588254862a6bb6105
Emre <emre@metaquark.de>
2013-11-29 09:28:39 +0100

    SVN test

---
+++ WindowsDemo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WindowsDemo.Resources;
+
+namespace WindowsDemo
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WindowsDemoTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace TestApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 5;
+            int b = 5;
+            int c = a + b;
+
+            Assert.AreEqual(10, c);
+        }
+    }
+}
